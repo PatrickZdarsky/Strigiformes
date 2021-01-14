eb470ad5fb40e1c7718d872b4d454b0569507bef
Rxcki <patrick.zdarsky@gmail.com>
2021-01-14 19:48:25 +0100

    Improve speed testing

--- strigiformes-core/src/main/java/net/nextvizion/strigiformes/Main.java
+++ strigiformes-core/src/main/java/net/nextvizion/strigiformes/Main.java
@@ -8,18 +8,24 @@ import net.nextvizion.strigiformes.parser.Parser;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+
+        int tries = 10000;
+        double time1 = 0, time2 = 0;
+
+
+        for (int i = 0; i < tries; i++) {
             var s1 = "${cloud:sysPrefix}%{§cRxcki|run_command:/pi Rxcki} befindet sich auf §{grey}Server %{§{gold}Lobby-§{white}1|run_command:/server lobby1} mit 6 anderen Spielern!";
             var colorParse = "§{green~random}I am a beautiful Gradient§{white}";
             var componentParse = "%{§cRxc§4ki|run_command:/pi Rxcki|show_text:§8This §7is a test\n§6new line}";
             long now = System.nanoTime();
-            Parser parser = new Parser("§6This is gold");
-            var message = parser.parse();
-            System.out.println("Time: "+(System.nanoTime()-now)/1000000.0+"ms");
+            var message = Parser.parse(s1+s1+s1+s1+s1+s1);
+            time1 += ((System.nanoTime()-now)/1000000.0);
             now = System.nanoTime();
-            System.out.println(message.toJson());
-            System.out.println("JsonTime: "+(System.nanoTime()-now)/1000000.0+"ms");
-            System.out.println("FINISH");
+            var json = message.toJson();
+            time2 += ((System.nanoTime()-now)/1000000.0);
         }
+
+        System.out.println("Parsing time avg: "+(time1/tries)+"ms");
+        System.out.println("Json time avg: "+(time2/tries)+"ms");
     }
 }
